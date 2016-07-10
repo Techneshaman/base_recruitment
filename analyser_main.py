02026ba0580217309fbfd4da40188c7477e7dbc7
Techneszaman <Techneszaman>
2016-07-10 12:35:48 +0200

    Questions 1 and 2 answers

--- analyser_main.py
+++ analyser_main.py
@@ -1,14 +1,17 @@
+__author__ = 'michal-witkowski'
+
 import pandas
 import numpy
 import matplotlib.pyplot
 
-# path = 'E:\\Workspace\\web_req_short.csv'
-path = 'E:\\Workspace\\web-requests-sample.csv'
+path = 'E:\\Workspace\\web_req_short.csv'
+# path = 'E:\\Workspace\\web-requests-sample.csv'
 data_frame = pandas.read_csv(path, sep=',')
 # data_frame = data_frame.drop(data_frame.columns[[0]], axis=1)
 # data_frame.request = data_frame.request.replace(to_replace='GET', value=numpy.nan)
 data_frame.requestTime = pandas.to_numeric(data_frame.requestTime, errors='coerce')
 print(data_frame.describe())
+
 
 def timestamp_to_hour(timestamp):
     ms_in_day = 86400000
@@ -16,10 +19,12 @@ def timestamp_to_hour(timestamp):
     hour = int((timestamp % ms_in_day - timestamp % ms_in_hour) / ms_in_hour)
     return hour
 
+
 def get_browser_producer(browser_version):
     browser_version = browser_version.split(' ')
     browser = browser_version[0]
     return browser
+
 
 def get_status_type(status_number):
     status_number = int(status_number)
@@ -32,6 +37,25 @@ def get_status_type(status_number):
     else:
         return 'Other'
 
+
+def find_highest_value(dictionary):
+    highest_key = -1
+    highest_value = 0
+    for key in dictionary:
+        value = dictionary[key]
+        if value > highest_value:
+            highest_key = key
+            highest_value = value
+    return [highest_key, highest_value]
+
+
+def count_distinct(data_series):
+    distinct_values = set()
+    for value in data_series:
+        distinct_values.add(value)
+    return len(distinct_values)
+
+
 data_frame['timestampDiff'] = data_frame['browser_timestamp'].sub(data_frame['timestamp'])
 data_frame['Browser_producer'] = data_frame['browser'].apply(get_browser_producer)
 data_frame['Status_type'] = data_frame['status'].apply(get_status_type)
@@ -39,10 +63,20 @@ data_frame['Status_type'] = data_frame['status'].apply(get_status_type)
 data_frame['timestamp'] = data_frame['timestamp'].apply(timestamp_to_hour)
 data_frame['browser_timestamp'] = data_frame['browser_timestamp'].apply(timestamp_to_hour)
 data_frame.rename(columns={'timestamp': 'hour', 'browser_timestamp': 'browser_hour'}, inplace=True)
-print(data_frame)
+# print(data_frame)
 # print(data_frame.dtypes)
 # print(data_frame.describe())
 
-user_pivot = data_frame.pivot_table(values='user_id', index='status', columns='Status_type', aggfunc='count')
-print(user_pivot)
-print(user_pivot.describe())
+active_users = data_frame['user_id']
+unique_users = count_distinct(active_users)
+print('ANSWER 1. There were', unique_users, 'unique users in the analysed period.')
+users_per_hour = data_frame.pivot_table(values='user_id', index='hour', aggfunc=lambda x: len(x.unique()))
+# print(users_per_hour)
+# print(users_per_hour.describe())
+
+users_per_hour = users_per_hour.to_dict()
+highest_activity = find_highest_value(users_per_hour)
+most_crowded_hour = highest_activity[0]
+most_crowded_hour_users_count = highest_activity[1]
+print('ANSWER 2. Most users were present at %s hours GMT-0 time. There were %s active users then.' % (most_crowded_hour,
+                                                                                most_crowded_hour_users_count))
